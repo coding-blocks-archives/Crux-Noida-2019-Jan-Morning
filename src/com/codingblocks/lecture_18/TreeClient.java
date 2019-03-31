07265c21f5554fbbd336fc4440413749e6fa4097
Keenwarrior <anujgargcse@gmail.com>
2019-03-31 16:42:49 +0530

    binary tree

--- src/com/codingblocks/lecture_18/TreeClient.java
+++ src/com/codingblocks/lecture_18/TreeClient.java
@@ -4,22 +4,22 @@ package com.codingblocks.lecture_18;
 public class TreeClient {
 
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
+        BinaryTreeGeneric<Integer> tree = new BinaryTreeGeneric<>();
 
-        tree.add("10");
-        tree.add("20", 'l');
-        tree.add("25", 'r');
-        tree.add("30", 'l', 'r');
-        tree.add("40", 'r', 'r');
+        tree.add(10);
+        tree.add(10, 'l');
+        tree.add(25, 'r');
+        tree.add(30, 'l', 'r');
+        tree.add(40, 'r', 'r');
 
-        tree.add("35", 'l', 'r', 'r');
+        tree.add(35, 'l', 'r', 'r');
 
         tree.display();
 
 
 //        System.out.println(tree.find("32"));\\
 
-        BinaryTree t = tree.deepMirror();
+        BinaryTreeGeneric<String> t = tree.deepMirror();
 
         System.out.println("---------------");
 

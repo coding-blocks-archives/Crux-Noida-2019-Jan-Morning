4d327ba3b3701fccbd083e30a825ecf037628439
Keenwarrior <anujgargcse@gmail.com>
2019-04-14 18:56:38 +0530

    graph

--- src/com/codingblocks/lecture_21/AdjListGraph.java
+++ src/com/codingblocks/lecture_21/AdjListGraph.java
@@ -14,6 +14,73 @@ public class AdjListGraph <E> {
         }
     }
 
+    public void connectedComponent(){
+
+        Stack<Vertex> stack = new Stack<>();
+        Set<Vertex> set = new HashSet<>();
+
+        for (Vertex start : vertices.values()){
+
+            if(set.contains(start)){
+                continue;
+            }
+
+            stack.push(start);
+            set.add(start);
+
+            while (!stack.empty()){
+                Vertex top = stack.pop();
+                System.out.print(top.value + " ");
+
+                for (Vertex padosi : top.neighbours){
+                    if (!set.contains(padosi)){
+                        stack.push(padosi);
+                        set.add(padosi);
+                    }
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public List<LinkedList<E>> connectedComponentList(){
+
+
+        List<LinkedList<E>> components = new LinkedList<>();
+        Stack<Vertex> stack = new Stack<>();
+        Set<Vertex> set = new HashSet<>();
+
+        for (Vertex start : vertices.values()){
+
+            if(set.contains(start)){
+                continue;
+            }
+
+            LinkedList<E> component = new LinkedList<>();
+            stack.push(start);
+            set.add(start);
+
+            while (!stack.empty()){
+                Vertex top = stack.pop();
+
+                component.add(top.value);
+
+                for (Vertex padosi : top.neighbours){
+                    if (!set.contains(padosi)){
+                        stack.push(padosi);
+                        set.add(padosi);
+                    }
+                }
+            }
+
+            components.add(component);
+        }
+
+        return components;
+    }
+
     public void DFT(E start){
         Vertex v = vertices.get(start);
 
@@ -36,6 +103,133 @@ public class AdjListGraph <E> {
         }
     }
 
+    public boolean bipartite(){
+
+
+        Set<Vertex> red = new HashSet<>();
+        Set<Vertex> black = new HashSet<>();
+        Stack<Vertex> stack = new Stack<>();
+
+        for (Vertex v : vertices.values()){
+            if (!red.contains(v) &&  ! black.contains(v)){
+                stack.push(v);
+                red.add(v);
+
+                while (!stack.empty()){
+                    Vertex top = stack.pop();
+
+                    for (Vertex padosi : top.neighbours){
+
+                        if (red.contains(top)){
+                            if (red.contains(padosi)) {
+                                return false;
+                            } else {
+                                if (!black.contains(padosi)){
+                                    black.add(padosi);
+                                    stack.push(padosi);
+                                }
+                            }
+                        } else {
+                            if (black.contains(padosi)) {
+                                return false;
+                            } else {
+                                if (!red.contains(padosi)){
+                                    red.add(padosi);
+                                    stack.push(padosi);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+
+        System.out.println(red);
+        System.out.println(black);
+
+
+        return true;
+    }
+
+    public boolean DFS(E start, E target){
+        Vertex v = vertices.get(start);
+
+        Stack<Vertex> stack = new Stack<>();
+        Set<Vertex> set = new HashSet<>();
+
+        stack.push(v);
+        set.add(v);
+
+        while (!stack.empty()){
+            Vertex top = stack.pop();
+
+            if (top.value.equals(target)){
+                return true;
+            }
+
+            for (Vertex padosi : top.neighbours){
+                if (!set.contains(padosi)){
+                    stack.push(padosi);
+                    set.add(padosi);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void BFT(E start){
+        Vertex v = vertices.get(start);
+
+        Queue<Vertex> queue = new LinkedList<>();
+        Set<Vertex> set = new HashSet<>();
+
+        queue.add(v);
+        set.add(v);
+
+        while (!queue.isEmpty()){
+            Vertex top = queue.remove();
+            System.out.println(top.value);
+
+            for (Vertex padosi : top.neighbours){
+                if (!set.contains(padosi)){
+                    queue.add(padosi);
+                    set.add(padosi);
+                }
+            }
+        }
+    }
+
+    public boolean BFT(E start, E target){
+        Vertex v = vertices.get(start);
+
+        Queue<Vertex> queue = new LinkedList<>();
+        Set<Vertex> set = new HashSet<>();
+
+        queue.add(v);
+        set.add(v);
+
+        while (!queue.isEmpty()){
+            Vertex top = queue.remove();
+
+            if (top.value.equals(target)){
+                return true;
+            }
+
+            for (Vertex padosi : top.neighbours){
+                if (!set.contains(padosi)){
+                    queue.add(padosi);
+                    set.add(padosi);
+                }
+            }
+        }
+
+        return false;
+    }
+
     public void addEdge(E first, E second){
         Vertex f = vertices.get(first);
         Vertex s = vertices.get(second);
@@ -53,6 +247,11 @@ public class AdjListGraph <E> {
 
         public Vertex(E value) {
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
         }
     }
 }
--- src/com/codingblocks/lecture_21/Client.java
+++ src/com/codingblocks/lecture_21/Client.java
@@ -1,5 +1,8 @@
 package com.codingblocks.lecture_21;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Client {
 
     public static void main(String[] args) {
@@ -8,7 +11,22 @@ public class Client {
         graph.addVertex(2);
         graph.addVertex(3);
         graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
 
-        graph.addEdge(1, 2);
+        graph.addEdge(1, 5);
+        graph.addEdge(3, 5);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 6);
+        graph.addEdge(4, 6);
+        graph.addEdge(2, 1);
+
+
+        System.out.println(graph.bipartite());
+
+
+//        List<LinkedList<Integer>> components = graph.connectedComponentList();
+//
+//        System.out.println(components);
     }
 }
